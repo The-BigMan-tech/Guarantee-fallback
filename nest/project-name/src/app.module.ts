58d77a1e70e1eceb87877103801d8d8e25b1c7a3
The-BigMan-tech <saddlebox9@gmail.com>
2024-12-05 12:38:12 +0100

    Some commmit

--- nest/project-name/src/app.module.ts
+++ nest/project-name/src/app.module.ts
@@ -6,9 +6,21 @@ import { experimentModule } from './experiment/experiment.module';
 import { experimentMiddleware,functionalMiddleware} from './experiment/experiment.middleware';
 import { blogModule } from './blog/blog.module';
 import {MongooseModule} from '@nestjs/mongoose'
+import { Connection } from 'mongoose';
 @Module(
   {
-    imports:[experimentModule,blogModule,MongooseModule.forRoot('mongodb://localhost:27017/MY_DATABASE')],
+    imports:[
+      experimentModule,blogModule,
+      MongooseModule.forRoot('mongodb://localhost:27017/MY_DATABASE',
+        {
+          onConnectionCreate:(connection:Connection)=>{
+              connection.on('connected',()=>console.log('Database connection successful'))
+              connection.on('error',()=>console.log('Database connection error'))
+              connection.on('disconnected',()=>console.log("Database disconnected"))
+          }
+        }
+      )
+    ],
     controllers: [AppController,SampleController,NewController],
     providers: [AppService],
   }
