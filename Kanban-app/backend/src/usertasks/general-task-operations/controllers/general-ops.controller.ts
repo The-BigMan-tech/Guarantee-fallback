afd20a9e8abdfef3e10f06cb4dc7d2dd268812cf
The-BigMan-tech <saddlebox9@gmail.com>
2024-12-09 10:57:39 +0100

    Added indexing for precision when editing a task

--- Kanban-app/backend/src/usertasks/general-task-operations/controllers/general-ops.controller.ts
+++ Kanban-app/backend/src/usertasks/general-task-operations/controllers/general-ops.controller.ts
@@ -1,15 +1,24 @@
-import { Controller,Post,Body,Delete,Query} from "@nestjs/common";
+import { Controller,Post,Body,Delete,Query,Put} from "@nestjs/common";
 import { TaskDTO } from "src/usertasks/dto/task.dto";
 import { TaskOperationsService } from "../services/general-ops.service";
+
 
 interface TaskLocation {
     boardName:string,
     taskInfo:TaskDTO
 }
+//*The possibility of two tasks having the same title brings up the use of its index for precision
 interface DeleteInfo {
     boardName:string,
     groupName:string,
+    index:number,
     title:string
+}
+interface EditInfo {
+    boardName:string,
+    groupName:string,
+    index:number,
+    newTask:TaskDTO
 }
 @Controller('tasks')
 export class TaskController {
@@ -24,7 +33,7 @@ export class TaskController {
     @Delete('/deleteTask')
     public async deleteTaskControl(@Query() task:DeleteInfo):Promise<string> {
         try {
-            const result = await this.operationService.deleteTask(task.boardName,task.groupName,task.title);
+            const result = await this.operationService.deleteTask(task.boardName,task.groupName,task.index,task.title);
             if (result === 'not found') {
                 return `CANNOT DELETE THE TASK ${task.title} FROM THE GROUP ${task.groupName} FROM THE BOARD ${task.boardName} BECAUSE THE TASK DOESNT EXIST`
             }
@@ -32,5 +41,10 @@ export class TaskController {
         }catch {
             return `CANNOT DELETE THE TASK ${task.title} FROM THE GROUP ${task.groupName} BECAUSE THE GROUP DOESNT EXIST`
         }
+    }
+    @Put('/editTask')
+    public async editTaskControl(@Body() task:EditInfo):Promise<string> {
+        await this.operationService.editTask(task.boardName,task.groupName,task.index,task.newTask);
+        return
     }
 }
--- Kanban-app/backend/src/usertasks/general-task-operations/services/general-ops.service.ts
+++ Kanban-app/backend/src/usertasks/general-task-operations/services/general-ops.service.ts
@@ -2,11 +2,21 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { BoardModelType } from "src/boards/schemas/board.schema";
 import { TaskDTO } from "src/usertasks/dto/task.dto";
+import { GroupDTO } from "src/groups/dto/groups.dto";
+import { BoardDefinition } from "src/boards/schemas/board.schema";
 
 @Injectable()
 export class TaskOperationsService {
     constructor(@InjectModel('Board') private BoardModel:BoardModelType) {
         //No implementation
+    }
+    public async doesTaskExist(group:GroupDTO,index:number,title:string):Promise<boolean> {
+        const task = group.tasks[index]
+        console.log("TASK",task);
+        if (task.title === title) {
+            return true
+        }
+        return false
     }
     public async addTask(boardName:string,groupName:string,taskInfo:TaskDTO):Promise<void> {
         await this.BoardModel.updateOne(
@@ -14,22 +24,23 @@ export class TaskOperationsService {
             { $push: { "groups.$.tasks": taskInfo } }     // Push the new task
         ).exec();
     }
-    public async deleteTask(boardName:string,groupName:string,title:string):Promise<string | void> {
-        //*The last parameter is to project only the group array containing only the group with the name that matches
-        const board = await this.BoardModel.findOne({name:boardName,"groups.name": groupName},{'groups.$':1}).exec()
+    public async deleteTask(boardName:string,groupName:string,index:number,title:string):Promise<string | void> {
+        //*The last parameter is to return the board document but should project only the group array containing only the group with the name that matches
+        const board:BoardDefinition = await this.BoardModel.findOne({name:boardName,"groups.name": groupName},{'groups.$':1}).exec()
         console.log(board);
-        const group = board.groups[0]
-
-        //*Find the index where the task we want to delete belongs,remove it through the splice method and update the board by setting the tasks array of the particular group with this new one.
-        const taskIndex = group.tasks.findIndex(task=> {task.title===title})
-        console.log("INDEX",taskIndex);
-        if (taskIndex === -1) {
+        const group:GroupDTO = board.groups[0]
+        const taskDoesNotExist = !(await this.doesTaskExist(group,index,title))
+        if (taskDoesNotExist) {
             return 'not found'
         }
-        group.tasks.splice(taskIndex,1)
+        group.tasks.splice(index,1)
         await this.BoardModel.updateOne(
             { name: boardName, "groups.name": groupName }, // Find the board and group
             { $set: { "groups.$.tasks":group.tasks } } // Use $pull to remove the task by title
         ).exec();
+    }
+    public async editTask(boardName:string,groupName:string,index:number,newTask:TaskDTO):Promise<void> {
+        const board = await this.BoardModel.findOne({name:boardName,"groups.name": groupName},{'groups.$':1}).exec()
+        const group = board.groups[0]
     }
 }
