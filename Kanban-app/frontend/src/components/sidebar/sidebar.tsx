842ecf5ba543d825650fc981772938cca1ed7d92
The-BigMan-tech <saddlebox9@gmail.com>
2024-12-10 17:31:40 +0100

    Introduced my frontend

--- Kanban-app/frontend/src/components/sidebar/sidebar.tsx
+++ Kanban-app/frontend/src/components/sidebar/sidebar.tsx
@@ -2,10 +2,10 @@
 import tw from 'tailwind-styled-components'
 
 const SideBarFlex = tw.aside`
-    bg-[#2c2c38] text-white w-[20%] h-[40rem]
+    flex flex-col items-center bg-[#2c2c38] text-white w-[20%] h-[40rem]
 `
 const BrandName = tw.h1`
-    font-bold text-3xl
+    font-bold text-3xl mt-4
 `
 export default function SideBar() {
     return (
