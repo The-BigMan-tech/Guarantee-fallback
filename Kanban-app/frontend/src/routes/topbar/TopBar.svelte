384daa9b5ea73e02e6ca86c19b00230883eba00c
The-BigMan-tech <saddlebox9@gmail.com>
2024-12-12 21:36:11 +0100

    A cool topbar design

--- Kanban-app/frontend/src/routes/topbar/TopBar.svelte
+++ Kanban-app/frontend/src/routes/topbar/TopBar.svelte
@@ -44,11 +44,19 @@
 </script>
 
 <div class='flex bg-[#2e2e3a] text-white h-20 items-center w-[62rem] relative border border-[#2e2e3a]'>
-    <h1 class='font-bold text-2xl ml-20'>{nameDisplay}</h1>
     {#if (nameDisplay !==  'Select a board to view its info')}
-        <button class='absolute right-56 bg-[rgb(100,94,197)] py-2 px-4 rounded-3xl'>+ Add new Task</button>
-        <button onclick={deleteBoard} class='absolute right-24'>
-            <h1>Delete this board</h1>
-        </button>
+        <div class='flex items-center relative top-4 left-12 w-28 h-6'>
+            <img class='w-28' src="/laps.svg" alt="">
+            <h1 class='font-bold relative bottom-4 right-20'>{nameDisplay}</h1>
+        </div>
+        <div class='flex absolute right-24 gap-8'>
+            <button class='bg-[#251e4f] py-3 px-4 rounded-3xl'>+ Add new Task</button>
+            <button onclick={deleteBoard} class='flex items-center gap-4'>
+                <img class='w-5' src="/trash-can-regular.svg" alt="">
+                <h1 class='text-[#f66473]'>Delete this board</h1>
+            </button>
+        </div>
+    {:else}
+        <h1 class='font-bold text-xl absolute left-16'>{nameDisplay}</h1>
     {/if}
 </div>
