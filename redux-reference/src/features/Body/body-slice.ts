425c8e3563fbee7d64273e3212f7c346bd993cb6
The-BigMan-tech <saddlebox9@gmail.com>
2024-12-23 22:50:47 +0100

    More redux code

--- redux-reference/src/features/Body/body-slice.ts
+++ redux-reference/src/features/Body/body-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice,PayloadAction} from '@reduxjs/toolkit'
+import { createSlice,PayloadAction,createAction} from '@reduxjs/toolkit'
 import { RootState } from '@/app/store'
 import { AppThunk } from '@/app/store'
 import { createAsyncThunk } from '@reduxjs/toolkit'
@@ -12,36 +12,39 @@ export interface CounterSliceState {
 const initialState:CounterSliceState = {
     value:0,
     loading:false,
-    error:''
+    error:'',
+    items:null
 }
 export const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
         increment(state,action:PayloadAction<number>):void {
-            state.value += action.payload
+            state.value += action.payload || 1
         }
     },
     extraReducers: (builder) => {
         builder
-            .addCase(fetchItems.pending, (state) => {
+            .addCase(fetchItems.pending,state => {
                 state.loading = true;
                 state.error = 'No error';
             })
-            .addCase(fetchItems.fulfilled, (state, action) => {
+            .addCase(fetchItems.fulfilled,(state, action) => {
                 state.loading = false;
                 state.items = action.payload; // store the fetched data
             })
-            .addCase(fetchItems.rejected, (state, action) => {
+            .addCase(fetchItems.rejected,state => {
                 state.loading = false;
-                state.error = action.error.message as string; // handle error
+                state.error = 'Error.No item could be found'; // handle error
             });
     },
 })
 export default counterSlice.reducer
-export const {increment} = counterSlice.actions
-export const selectCountFrom = (store:RootState):number => store.counter.value
+export const {} = counterSlice.actions
+export const increment = createAction<number | undefined>('counter/increment')
 
+export const selectCountFrom = (store:RootState):number => store.counter.value;
+export const selectLoadingFrom = (store:RootState):boolean => store.counter.loading
 
 export function incrementIfOdd(amount:number): AppThunk {
     return (dispatch, getState) => {
@@ -53,8 +56,8 @@ export function incrementIfOdd(amount:number): AppThunk {
     }
 }
 export const fetchItems = createAsyncThunk('counter/fetchItems',
-    async (itemId: string) => {
-        const item = await fetch(itemId)
-        return item
+    async (itemId: string):Promise<string> => {
+        const response:Response = await fetch(itemId)
+        return await response.json()
     }
 )
--- redux-reference/src/features/Body/body.tsx
+++ redux-reference/src/features/Body/body.tsx
@@ -1,14 +1,22 @@
-import {increment,incrementIfOdd} from './body-slice'
+import {increment,incrementIfOdd,fetchItems} from './body-slice'
 import {state,useAppDispatch} from "@/app/hooks"
-import { selectCountFrom } from './body-slice'
+import { selectCountFrom,selectLoadingFrom} from './body-slice'
+import { useEffect } from 'react'
 
 export default function Body() {
     const dispatch = useAppDispatch()
-    const count:number = state((store)=>selectCountFrom(store))
+    const count:number = state(store=>selectCountFrom(store))
+    const loading:boolean = state(store=>selectLoadingFrom(store))
+
+    useEffect(()=>{
+        dispatch(fetchItems(''))
+    },[dispatch])
+
+    if (loading) return <p>Loading...</p>;
 
     function increase() {
-        dispatch(increment(1))
-        dispatch(incrementIfOdd(1))
+        dispatch(increment())
+        dispatch(incrementIfOdd(2))
     }
     return (
         <>
