49efd0dc3e1393ffc782bfdd1845fbdbf822a7b9
The-BigMan-tech <saddlebox9@gmail.com>
2025-01-06 22:51:49 +0100

    Added a table printer

--- postgres-sql-ref/print-table.js
+++ postgres-sql-ref/print-table.js
@@ -1,4 +1,14 @@
 import postgres from 'postgres'
+import { json2csv } from 'json-2-csv';
+import * as fs from 'fs'
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const sql = postgres({
     host: 'localhost',      // PostgreSQL server address
     port: 5432,             // PostgreSQL server port
@@ -7,7 +17,9 @@ const sql = postgres({
     database: 'school', // The name of your database
 })
 async function fetchData() {
-    const rows = await sql`SELECT * FROM students LIMIT 100`;
-    console.log(rows);
+    const table = await sql`SELECT * FROM students LIMIT 100`;
+    const csv = await json2csv(table);
+    await fs.writeFile(`${__dirname}/table.csv`, csv,(err)=>{console.log('ERROR',err);})
 }
-fetchData()
+
+await fetchData();
