1fc6d5d5be4811a946841bfcd16b8e5e3e3915b5
The-BigMan-tech <saddlebox9@gmail.com>
2025-01-06 23:46:55 +0100

    Added more notes

--- postgres-sql-ref/insert-query.sql
+++ postgres-sql-ref/insert-query.sql
@@ -2,6 +2,6 @@ INSERT INTO public.students
     (student_name,student_age,student_class,haspaidfully,subjects,scores)
 VALUES
     (
-        'bolaji',20,'ss3',true,ARRAY['English','Mathematics','Physics'],
+        'tolani',20,'ss3',true,ARRAY['English','Mathematics','Physics'],
         ROW(100,70,60,40,NULL)
     );
