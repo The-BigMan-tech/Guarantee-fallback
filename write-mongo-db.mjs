1c23584fd771bd7a3f8c1545c9c3f0e90bf092c8
The-BigMan-tech <saddlebox9@gmail.com>
2025-01-07 16:25:23 +0100

    Script is working

--- write-mongo-db.mjs
+++ write-mongo-db.mjs
@@ -9,7 +9,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
+export async function connectToDB() {
+    try {
+        const client = new MongoClient("mongodb://localhost:27018/");
+        const database = client.db('Kanbans');
+        return database
+    }catch(error) {
+        console.log(`Database connection error: ${error}`);
+    }
+}
+export async function returnCollection(name) {
+    const db = await connectToDB()
+    const collections = await db.listCollections({ name:name}).toArray();
+    if (!collections.length ) return await db.createCollection(name)
+    return await db.collection(name)
+}
 
 const collection = await returnCollection('boards')
-const data = JSON.stringify(await taskDataCollection.find({}).toArray())
+const data = JSON.stringify(await collection.find({}).toArray())
 await fs.writeFile(`${__dirname}/mongo.json`,data,(err)=>{console.log('ERROR',err);})
