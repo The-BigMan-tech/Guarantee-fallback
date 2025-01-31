5a160115e830d57b86e39894baa89338a965753e
The-BigMan-tech <saddlebox9@gmail.com>
2025-01-31 16:50:49 +0100

    Started using astro

--- astro-ref/src/App.tsx
+++ astro-ref/src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [count,setCount] = useState(0)
   return (
     <>
     <img className='w-10 h-10' src="/react.svg" alt="" />
     <h1 className='text-red-500'>hello</h1>
     <p>HH</p>
+    <h1>Current count: {count}</h1>
+    <button className='cursor-pointer' onClick={()=>setCount(prev=>prev+1)}>Increment count</button>
     </>
   )
 }
