13cf85327db9f5a4ed73920e0bceed287be029fd
The-BigMan-tech <saddlebox9@gmail.com>
2025-02-06 11:05:22 +0100

    Added next js with electron and some notes

--- Mongoose-ref/database/connection.ts
+++ Mongoose-ref/database/connection.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-
 export async function connectToDB() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/MongoData')
---
+++ next-ref/electron.mjs
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  win.loadURL('http://localhost:3000')
+}
+app.whenReady().then(() => {
+  createWindow()
+})
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
