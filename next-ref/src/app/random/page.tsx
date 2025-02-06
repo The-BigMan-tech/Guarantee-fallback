39ff97f36069a2e81422212ea5c489ad112d0476
The-BigMan-tech <saddlebox9@gmail.com>
2025-02-06 15:38:19 +0100

    Committed

---
+++ next-ref/src/app/random/page.tsx
@@ -0,0 +1,5 @@
+import Random from "@/components/random";
+
+export default function RandomPage(){
+    return <Random/>
+}
---
+++ next-ref/src/components/random.tsx
@@ -0,0 +1,7 @@
+export default function Random() {
+    return (
+        <>
+            <h1 className="font-roboto">Random</h1>
+        </>
+    )
+}
