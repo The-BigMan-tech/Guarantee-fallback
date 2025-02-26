ed177bf8b63064d0128eea02c94e4d876a7ae9ad
The-BigMan-tech <saddlebox9@gmail.com>
2025-02-26 10:10:39 +0100

    added notes to the algorithm

--- all-notes/squeezepack/new.ts
+++ all-notes/squeezepack/new.ts
@@ -1,7 +1,7 @@
 import { Tiny } from "./tiny.ts";
 
 const scores = new Tiny('scores',true)
-scores.data = [22,2345678910]
+scores.data = [22]
 console.log('Scores data:',scores.data.then(()=>{
     console.log('Compressed data: ',scores.state)
     console.log(scores.at(-1));
