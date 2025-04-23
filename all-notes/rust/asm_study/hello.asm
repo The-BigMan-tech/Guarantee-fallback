3789ebd622c298198dd4844f7aff23cceb6c3570
The-BigMan-tech <saddlebox9@gmail.com>
2025-04-23 23:19:06 +0100

    learnt more about assembly

---
+++ all-notes/rust/asm_study/hello.asm
@@ -0,0 +1,24 @@
+segment .data
+    name DB 'Person',13,10,0
+    person_len EQU $ - name
+
+segment .text
+    win_api:global main
+    extern ExitProcess
+    extern WriteConsoleA
+    extern GetStdHandle
+
+    main:
+        
+    syswrite:sub RSP,40
+    mov ECX,-11             
+    call GetStdHandle
+
+    logic:mov RCX,RAX                
+    lea RDX,[rel name]    
+    mov R8D,person_len     
+    mov R9D,0
+    call WriteConsoleA
+
+    exit:xor ECX,ECX        
+    call ExitProcess
