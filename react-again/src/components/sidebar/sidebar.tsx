2210079606415e2ba1be7d2ef082d8206f220789
The-BigMan-tech <saddlebox9@gmail.com>
2025-05-04 15:08:57 +0100

    made some changes to the godotbot

--- react-again/src/components/sidebar/sidebar.tsx
+++ react-again/src/components/sidebar/sidebar.tsx
@@ -2,10 +2,10 @@ export default function Sidebar() {
     return (
         <div className="flex flex-col bg-[#282a4a] h-[98%] w-[22%] border-r-2 border-slate-400">
             <div className="border-b-2 border-slate-400 h-[9.5%] content-center">
-                <h1 className="font-semibold font-[Consolas]  relative left-[2%] sm:text-sm lg:text-lg">File manager</h1>
+                <h1 className="font-semibold font-[Consolas]  relative left-[2%] sm:text-sm lg:text-lg text-transparent">File manager</h1>
             </div>
             <div className="ml-2">
-                <h1 className="ont-[Verdana]">My Files</h1>
+                <h1 className="text-transparent">My Files</h1>
             </div>
         </div>
     )
--- react-again/src/components/top/action-pane/action-pane.tsx
+++ react-again/src/components/top/action-pane/action-pane.tsx
@@ -1,6 +1,6 @@
 export default function ActionPane() {
     return (
-        <div className="bg-[#323552] w-full border-b border-slate-400 shadow-sm py-[1.4%]">
+        <div className="bg-[#323552] w-full border-b border-slate-400 shadow-sm py-[1%]">
             <h1 className="text-transparent">Hello action bar</h1>
         </div>
     )
