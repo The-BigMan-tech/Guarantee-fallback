c024ea5545d88a867952c1c531275de06a4e461e
The-BigMan-tech <saddlebox9@gmail.com>
2025-05-06 18:59:43 +0100

    tweaked the fsnode key

--- react-again/src/components/body/fs-node.tsx
+++ react-again/src/components/body/fs-node.tsx
@@ -1,6 +1,6 @@
 import { FsNode } from "../../utils/rust-fs-interface"
 
-export default function FsNodeComponent(props:{fsNode:FsNode}) {
+export default function FsNodeComponent(props:{key:string,fsNode:FsNode}) {
     function truncateName(name:string):string {
         return (name.length < 16)?name:`${name.slice(0,16)}...`
     }
@@ -14,7 +14,7 @@ export default function FsNodeComponent(props:{fsNode:FsNode}) {
         }
     }
     return (
-        <button className="flex flex-col items-center justify-center gap-2 cursor-pointer">
+        <button key={props.key} className="flex flex-col items-center justify-center gap-2 cursor-pointer">
             <img className={`${fixIconSize(props.fsNode.primary.iconPath)}`} src={`./assets/file-icons/${props.fsNode.primary.iconPath}`} alt="" />
             <h1 className="text-sm font-sans mb-5">{truncateName(props.fsNode.primary.nodeName)}</h1>
         </button>
