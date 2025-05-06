51b5fe9c6e8999c7fa9e6e5414d922a7ce831edf
The-BigMan-tech <saddlebox9@gmail.com>
2025-05-06 18:38:43 +0100

    small changes to the body styles

--- react-again/src/components/body/body.tsx
+++ react-again/src/components/body/body.tsx
@@ -9,10 +9,10 @@ export default function Body() {
         <>
             <div className="h-[100%] bg-[#1f1f30] w-[90%] shadow-md rounded-md">
                 {fsNodes
-                    ?<div className="grid sm:grid-cols-4 md:grid-cols-5 h-auto max-h-[96%] gap-x-[1.2%] gap-y-[5%] mt-[1.5%] ml-[1.5%] w-[99%] overflow-y-scroll overflow-x-hidden items-center justify-center">
+                    ?<div className="grid sm:grid-cols-4 md:grid-cols-5 h-auto max-h-[96%] gap-x-[1.2%] gap-y-[5%] mt-[2%] ml-[1.5%] w-[99%] overflow-y-scroll overflow-x-hidden items-center justify-center">
                         {fsNodes.map((fsNode)=><FsNodeComponent {...{fsNode}}/>)}
                     </div>
-                    :<h1>Empty</h1>
+                    :<h1 className="self-center justify-self-center relative top-[40vh] text-2xl font-[Consolas]">Empty</h1>
                 }
             </div>
         </>
