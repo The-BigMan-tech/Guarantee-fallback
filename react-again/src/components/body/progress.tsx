13d559f8ef1b1935c502ad5581f5d8924959e9b5
The-BigMan-tech <saddlebox9@gmail.com>
2025-05-15 00:54:03 +0100

    added a key

--- react-again/src/components/body/progress.tsx
+++ react-again/src/components/body/progress.tsx
@@ -19,7 +19,7 @@ export default function Progress({searchProgress}:{searchProgress:Record<string,
                 ?<p>No active search progress.</p>
                 :<div className="flex flex-col text-sm gap-5 ">
                     {Object.entries(searchProgress).map(([path, progress]) => 
-                        <div>
+                        <div key={path}>
                             <h1>Current path: {truncateStart(path,20)}</h1>
                             <h1>Progress: {progress.lastThreshold}</h1>
                         </div>
