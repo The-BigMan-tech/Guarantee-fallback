ce6a93cb50f1930ab38d91934b0b649e72fac198
The-BigMan-tech <saddlebox9@gmail.com>
2025-05-15 10:52:00 +0100

    changed some styles

--- react-again/src/components/top/upper-top/counter.tsx
+++ react-again/src/components/top/upper-top/counter.tsx
@@ -10,10 +10,22 @@ export default function Counter() {
     const [progress,setProgress] = useState<NodeCount[]>([]);
     const uniqueProgress = useMemo(()=>progress.map(node=>({ id: uniqueID(),data:node})),[progress])
 
+    function truncateStart(str:string, maxLength:number) {
+        if (str.length <= maxLength) {
+            return str; 
+        }
+        return '...' + str.slice(str.length - (maxLength - 3));
+    }
+
     useEffect(()=>{
         if (nodeCount.save) {
             console.log("NODE COUNT LENGTH IS ZERO");
-            setProgress(prev=>[...prev,nodeCount]);
+            setProgress(prev=>{
+                if (prev.length > 7) {
+                    prev.length = 0
+                }
+                return [...prev,nodeCount]
+            });
         }
         if ((nodeCount.totalItems == 0 ) && (nodeCount.items == 0)) {
             setProgress([])
@@ -25,9 +37,12 @@ export default function Counter() {
     },[uniqueProgress])
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col absolute right-5">
             {uniqueProgress.map((node)=>
-                <h1 key={node.id}>Searched: {node.data.items} / {node.data.totalItems} items</h1>
+                <div className="flex flex-col mb-4">
+                    <h1>Path: {truncateStart(node.data.path,20)}</h1>
+                    <h1 key={node.id}>Searched: {node.data.items} / {node.data.totalItems} items</h1>
+                </div>
             )}
             {(!(quickSearch) && !(isSearchTerminated))
                 ?<h1>Searched: {nodeCount.items} / {nodeCount.totalItems} items</h1>
