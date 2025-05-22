c571cd34400c30ef4f9afdd9d45def133a96174d
The-BigMan-tech <saddlebox9@gmail.com>
2025-05-22 19:57:04 +0100

    changed the scoring function

--- react-again/src/redux/processingSlice.ts
+++ react-again/src/redux/processingSlice.ts
@@ -10,6 +10,7 @@ import Fuse, { FuseResult } from 'fuse.js';
 import { toast,ToastOptions,Bounce,Flip,Zoom} from 'react-toastify';
 import { Heap } from 'heap-js';
 import {distance} from "fastest-levenshtein"
+import fuzzysort from 'fuzzysort'
 
 const fuseOptions = {
     keys: ['primary.nodeName','primary.fileExtension',],
@@ -576,16 +577,6 @@ function aggressiveFilter(str:string | null,query:string):boolean {
     }
     return false
 }
-export function isSubsequence(str:string,query:string):boolean {//im not using it yet since i changed to another function for the same purpose but ill still leave it
-    const normalizedStr = normalizeString(str);
-    const normalizedQuery = normalizeString(query);
-    let i = 0, j = 0;
-    while (i < normalizedStr.length && j < normalizedQuery.length) {
-        if (normalizedStr[i] === normalizedQuery[j]) j++;
-        i++;
-    }
-    return j === normalizedQuery.length;
-}
 export function toggleQuickSearch():AppThunk {
     return (dispatch,getState)=>{
         const quickSearch:boolean = selectQuickSearch(getState());
@@ -602,7 +593,8 @@ function getMatchScore(query:string,str:string,minThreshold:number):number {
     if (adjustedScore > 0) {//what i did here is that i didnt want to tolerate low matches.without the -35,license.txt will match space but with it space_mono will only match space by 15% when it should be 50% so what i did,was that the prev adjusted value was just used to remove typo big results by taking them to zero,after that the ones that passed will have the min threshold which is 35 added back to them again to get their desreved score
         adjustedScore += minThreshold
     }
-    return adjustedScore;
+    const subsequenceResult = fuzzysort.single(query,str)
+    return Math.max(adjustedScore,subsequenceResult?.score || 0);
 }
 //*This is the new async thunk pattern ill be using from hence forth,ill refactor the old ones once ive finsihed the project
 function searchInBreadth(rootPath:string,searchQuery:string,heavyFolderQueue:string[],processHeavyFolders:boolean,startTime:number):AppThunk<Promise<void>> {
@@ -683,13 +675,13 @@ function searchInBreadth(rootPath:string,searchQuery:string,heavyFolderQueue:str
                         const normalizedStr = normalizeString(awaitedNode.primary.nodeName + (awaitedNode.primary.fileExtension || ""));//i normalized both the query and the target string to increase fuzziness
                         const normalizedQuery = normalizeString(searchQuery);
 
-                        const matchScore = getMatchScore(normalizedQuery,normalizedStr,35);
+                        const matchScore = getMatchScore(normalizedQuery,normalizedStr,20);
+                        console.log("MATCH SCORE","NODE:",awaitedNode.primary.nodeName,"|SCORE:",matchScore);
                         if (matchScore > 0) {//the number of matches increases qualitative relevance
                             relevantNodes += 1;
                             relevancePercent = roundToTwo( (relevantNodes / totalNodes) * 100 )//to ensure that the relevance percent is always updated upon looping
                             processImmediately = (relevancePercent >= relevanceThreshold) || (matchScore >= matchPercentThreshold)//the match score is used to check for the quality of the relevance as usual
                             
-                            console.log("MATCH SCORE FOR A NODE FROM:",currentSearchPath,"NODE:",awaitedNode.primary.nodeName,"SCORE:",matchScore);
                             if (processImmediately) {
                                 console.log("SEARCH PATH:",currentSearchPath,"IS BEING PROCESSED IMMEDIATELY");
                                 break//early termination once enough relevance has been reached
