be57c8a83ef830e2082fc90aa0563a82aae42d4e
The-BigMan-tech <saddlebox9@gmail.com>
2025-06-05 13:08:39 +0100

    added storybook js

---
+++ three-js-ref/.storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
+};
+export default config;
