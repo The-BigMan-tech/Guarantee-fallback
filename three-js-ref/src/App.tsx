d5c701109f35083c89ce31ec0c377b4a1fa57173
The-BigMan-tech <saddlebox9@gmail.com>
2025-06-06 16:59:39 +0100

    just a note

--- three-js-ref/src/App.tsx
+++ three-js-ref/src/App.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react'
+import * as THREE from 'three';
+import {cube} from "./three-scenes/cube.three"
+import animateCube from './three-scenes/cube.three';
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-      <button>A sample button</button>
-    </>
-  )
+    useEffect(()=>{
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+        const renderer = new THREE.WebGLRenderer();
+
+        scene.add(cube)
+        camera.position.z = 5;
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        renderer.setAnimationLoop(()=>{
+            animateCube()
+            renderer.render( scene, camera );
+        });
+    },[])
+
+    return (
+        <>
+        
+        </>
+    )
 }
 export default App
---
+++ three-js-ref/src/three-scenes/cube.three.ts
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+export const cube = new THREE.Mesh( geometry, material )
+
+export default function animateCube() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+}
