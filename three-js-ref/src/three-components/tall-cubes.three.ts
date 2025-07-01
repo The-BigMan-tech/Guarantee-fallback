58c2b513052e5a626b087670e0e2959482b1abdc
The-BigMan-tech <saddlebox9@gmail.com>
2025-07-01 23:46:04 +0100

    added notes

--- three-js-ref/src/three-components/tall-cubes.three.ts
+++ three-js-ref/src/three-components/tall-cubes.three.ts
@@ -18,8 +18,8 @@ const points = pds.fill(); // array of [x, z] points
 export const cubesGroup = new THREE.Object3D();
 
 const tallCubeMaterial = new THREE.MeshPhysicalMaterial({ color:0x4f4f4f});
-const minHeight = 7;
-const maxHeight = 7;
+const minHeight = 2;
+const maxHeight = 4;
 
 const width = 40
 for (let i = 0; i < points.length; i++) {
