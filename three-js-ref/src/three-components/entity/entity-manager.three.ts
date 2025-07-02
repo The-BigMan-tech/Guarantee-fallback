d8d84ce371cb674f1b1a5cb78b5fb7ca9d811398
The-BigMan-tech <saddlebox9@gmail.com>
2025-07-02 22:54:47 +0100

    respawned entities after death

--- three-js-ref/src/three-components/entity/entity-manager.three.ts
+++ three-js-ref/src/three-components/entity/entity-manager.three.ts
@@ -7,39 +7,44 @@ import { Entity,entities} from "./entity.three";
 
 export const entityGroup:THREE.Group = new THREE.Group();
 
-for (let i = 0;i <= 1;i++) {
-    const entityFixedData:FixedControllerData = {
-        modelPath:'./silvermoon.glb',
-        spawnPoint: new RAPIER.Vector3(0,20,-10),
-        characterHeight:2,
-        characterWidth:1,
-        shape:'capsule',
-        mass:40,
+function spawnEntities() {
+    for (let i = 0;i < 1;i++) {
+        const entityFixedData:FixedControllerData = {
+            modelPath:'./silvermoon.glb',
+            spawnPoint: new RAPIER.Vector3(0,20,-10),
+            characterHeight:2,
+            characterWidth:1,
+            shape:'capsule',
+            mass:40,
+        }
+        const entityDynamicData:DynamicControllerData = {
+            horizontalVelocity:20,
+            jumpVelocity:27,
+            jumpResistance:6,
+            rotationDelta:0.05,
+            rotationSpeed:0.2,
+            maxStepUpHeight:2,
+            gravityScale:1
+        }
+        const entityMiscData:EntityMiscData = {
+            targetController:player,
+            targetHealth:player.health,
+            healthValue:10,
+            attackDamage:0
+        }
+        const managingStruct:ManagingStructure = {
+            group:entityGroup,
+            entities:entities
+        }
+        const entity = new Entity(entityFixedData,entityDynamicData,entityMiscData,managingStruct);
+        entities.push(entity);
+        entityGroup.add(entity.controller);
+        entityGroup.add(entity.points);//add the points to the scene when the controller is added to the scene which ensures that this is called after the scene has been created)
     }
-    const entityDynamicData:DynamicControllerData = {
-        horizontalVelocity:20,
-        jumpVelocity:27,
-        jumpResistance:6,
-        rotationDelta:0.05,
-        rotationSpeed:0.2,
-        maxStepUpHeight:2,
-        gravityScale:1
-    }
-    const entityMiscData:EntityMiscData = {
-        targetController:player,
-        targetHealth:player.health,
-        healthValue:10,
-        attackDamage:0
-    }
-    const managingStruct:ManagingStructure = {
-        group:entityGroup,
-        entities:entities
-    }
-    const entity = new Entity(entityFixedData,entityDynamicData,entityMiscData,managingStruct);
-    entities.push(entity);
-    entityGroup.add(entity.controller);
-    entityGroup.add(entity.points);//add the points to the scene when the controller is added to the scene which ensures that this is called after the scene has been created)
 }
 export function updateAllEntities() {
     entities.forEach(entity => entity.updateController());
+    if (entities.length == 0) {
+        spawnEntities()
+    }
 }
