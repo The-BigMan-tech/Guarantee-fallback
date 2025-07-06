e5a8db32da354f799f05881d6531a29c5e03a403
The-BigMan-tech <saddlebox9@gmail.com>
2025-07-06 20:24:39 +0100

    another soft refactor to my choice func

--- three-js-ref/src/three-components/entity/choices.ts
+++ three-js-ref/src/three-components/entity/choices.ts
@@ -8,7 +8,7 @@ function accum(weights:number[]):number[] {
     console.log('Accum list: ',accumList)
     return accumList
 }
-function bisect_right(arr:number[], x:number,endIndex:number) {
+function bisectRight(arr:number[], x:number,endIndex:number) {
     let startIndex:number = 0;
     while (startIndex < endIndex) {
         const mid = Math.floor((startIndex + endIndex)/2);
@@ -17,18 +17,18 @@ function bisect_right(arr:number[], x:number,endIndex:number) {
     }
     return startIndex
 }
-export function choices(population:number[],weights:number[],numOfResults:number):number[] {//it will return an empty array if the weights are empty
+export function choices<T>(population:T[],weights:number[],numOfResults:number):T[] {//it will return an empty array if the weights are empty
     const cumWeights:number[] = accum(weights);
     const totalWeight = cumWeights[cumWeights.length-1];
-    const results:number[] = [];
-    for (let i=0;i<numOfResults;i++) {
-        const lastIndex = population.length-1;
-        const randomWeight = Math.random() * totalWeight;//declared under the loop formulti choice selection
-        const num = population[bisect_right(cumWeights,randomWeight,lastIndex)];
+    const results:T[] = [];
+    for (let i=0; i < numOfResults; i++) {
+        const lastIndex:number= population.length-1;
+        const randomWeight:number = Math.random() * totalWeight;//declared under the loop formulti choice selection
+        const num:T = population[bisectRight(cumWeights,randomWeight,lastIndex)];
         results.push(num);
     }
     return results;
 }
 const x = [1,2,3]
-const y = choices(x,[1,1,1],2)
+const y = choices(x,[1,8,10],2)
 console.log("y: ",y);
