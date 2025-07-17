7ce91b2e995f016b2dafa075957ff0d840e3fc06
The-BigMan-tech <saddlebox9@gmail.com>
2025-07-17 17:40:53 +0100

    added svgs

--- three-js-ref/src/three-components/item-system/item-gui.tsx
+++ three-js-ref/src/three-components/item-system/item-gui.tsx
@@ -186,7 +186,12 @@ export default function ItemGui() {
                 ?<>
                     <motion.div key="div1" className="absolute z-20 top-[2%] left-[4%]" {...ANIMATION_CONFIG.buttonDiv}>
                         <motion.button className=" w-[9.5vw] py-[4%] shadow-sm cursor-pointer bg-[#5858588e] hover:bg-[#48372bb1] font-[Consolas] font-bold text-[#fffffffd] " onClick={toggleTab} {...ANIMATION_CONFIG.button}>
-                            {hovered ? "Switch" : tab }
+                            {hovered 
+                                ? "Switch" 
+                                :<>
+                                    {tab=="Items"?<img/>:<img/>}
+                                </>
+                            }
                         </motion.button>
                     </motion.div>
 
