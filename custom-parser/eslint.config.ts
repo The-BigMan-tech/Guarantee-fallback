3b72d2dc1eee3e7e34b4852447fff3fa4b8fd53a
The-BigMan-tech <saddlebox9@gmail.com>
2025-08-13 10:19:45 +0100

    upgraded my configs

--- custom-parser/eslint.config.ts
+++ custom-parser/eslint.config.ts
@@ -3,96 +3,109 @@ import tsEslint from 'typescript-eslint';
 import globals from 'globals'; // npm package that exports common global vars
 import importPlugin from "eslint-plugin-import";
 
+const srcFiles =  [
+    'src/**/*.ts', 
+    'src/**/*.tsx',
+    'src/**/*.js', 
+    'src/**/*.jsx',
+    'tests/**/*.ts', 
+    'tests/**/*.tsx',
+    'tests/**/*.js', 
+    'tests/**/*.jsx'
+]
 const config = tsEslint.config(
-  eslint.configs.recommended,
-  tsEslint.configs.recommended,
-  tsEslint.configs.strict,
-  tsEslint.configs.stylistic,
-  { // Apply environment globals for JavaScript files
-    files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+    eslint.configs.recommended,
+    tsEslint.configs.recommended,
+    tsEslint.configs.strict,
+    tsEslint.configs.stylistic,
+    {
+        ignores: ["eslint.config.ts","vitest.config.ts","./build/**","./dist/**"],
     },
-  },
-  {
-    ignores: ["eslint.config.ts","vitest.config.ts"],
-  },
-  {
-    rules:{//i used warnings to prevent noise that can distract the dev from actual type errors
-      'indent':['warn', 'tab'],
-      'no-mixed-spaces-and-tabs': 'warn',
-      'semi': ['warn', 'always'],
+    { // Apply environment globals for JavaScript files
+        files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },  
     },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx','**/*.js', '**/*.jsx'], // Apply only for TS files
-    rules: {
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-extraneous-class": [
-        "warn", { 
-          "allowStaticOnly": true 
+    {
+        files:srcFiles, 
+        rules:{//i used warnings to prevent noise that can distract the dev from actual type errors
+            'indent':['warn',4],
+            'no-mixed-spaces-and-tabs': 'warn',
+            'semi': ['warn', 'always'],
+        },
+    },
+    {
+        files:srcFiles, 
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "error",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/no-extraneous-class": [
+                "warn", { 
+                    "allowStaticOnly": true 
+                }
+            ],
+            "@typescript-eslint/naming-convention": [
+                "warn",
+                {
+                    "selector": "variable",
+                    "modifiers": ["const"],
+                    "format": ["camelCase", "UPPER_CASE"]
+                },
+                {
+                    "selector": "variable",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "function",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "class",
+                    "format": ["PascalCase"]
+                },
+                {
+                    "selector": "classMethod",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "classProperty",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "classProperty",
+                    "modifiers": ["readonly"],
+                    "format": ["UPPER_CASE"]
+                }
+            ]   
         }
-      ],
-      "@typescript-eslint/naming-convention": [
-        "warn",
-        {
-          "selector": "variable",
-          "modifiers": ["const"],
-          "format": ["camelCase", "UPPER_CASE"]
+    },
+    {
+        files:srcFiles,
+        plugins: {
+            import: importPlugin,
         },
-        {
-          "selector": "variable",
-          "format": ["camelCase"]
+        rules: {
+            "import/extensions": [
+                "error",
+                "ignorePackages",
+                {
+                    "js": "always",
+                    "ts": "never",
+                    "tsx": "never"
+                }
+            ]
         },
-        {
-          "selector": "function",
-          "format": ["camelCase"]
-        },
-        {
-          "selector": "class",
-          "format": ["PascalCase"]
-        },
-        {
-          "selector": "classMethod",
-          "format": ["camelCase"]
-        },
-        {
-          "selector": "classProperty",
-          "format": ["camelCase"]
-        },
-        {
-          "selector": "classProperty",
-          "modifiers": ["readonly"],
-          "format": ["UPPER_CASE"]
+        settings: {
+            "import/resolver": {
+                "node": {
+                    "extensions": [".js", ".jsx", ".ts", ".tsx"]
+                }
+            }
         }
-      ]
     }
-  },
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      "import/extensions": [
-        "error",
-        "ignorePackages",
-        {
-          "js": "always",
-          "ts": "never",
-          "tsx": "never"
-        }
-      ]
-    },
-    settings: {
-      "import/resolver": {
-        "node": {
-          "extensions": [".js", ".jsx", ".ts", ".tsx"]
-        }
-      }
-    }
-  }
 );
 export default config as any;//the cast is a workaround till the project maintainers resolve type issues
