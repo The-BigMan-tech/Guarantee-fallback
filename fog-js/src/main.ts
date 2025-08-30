19ea4852c9a823b9037a2f20e8b631cf1207fae9
The-BigMan-tech <saddlebox9@gmail.com>
2025-08-30 18:45:37 +0100

    updated the type system of my binding

--- fog-js/src/main.ts
+++ fog-js/src/main.ts
@@ -41,17 +41,17 @@ function resolutionErr(result:Result):boolean {
  * For the .json file,it directly loads the data onto the server document but for the .fog file,it resolves the document to the provided output folder and loads the data onto the server
  */
 
-export async function importDocFromPath<U extends string,T extends PatternedAtomList>(filePath:string,outputFolder?:string):Promise<Doc<U,T> | undefined> {
+export async function importDocFromPath<M extends Atom,P extends string,K extends string>(filePath:string,outputFolder?:string):Promise<Doc<M,P,K> | undefined> {
     const result = await client.request("importDocFromPath",{filePath,outputFolder}) as Result;
     if (resolutionErr(result)) return;
     console.log(chalk.green('\nSuccessfully loaded the document onto the server.'));
-    return new Doc<U,T>();
+    return new Doc<M,P,K>();
 }
-export async function importDocFromObject<U extends string,T extends PatternedAtomList>(obj:Record<string,any>):Promise<Doc<U,T> | undefined> {
+export async function importDocFromObject<M extends Atom,P extends string,K extends string>(obj:Record<string,any>):Promise<Doc<M,P,K> | undefined> {
     const result = await client.request("importDocFromObject",{obj}) as Result;
     if (resolutionErr(result)) return;
     console.log(chalk.green('\nSuccessfully loaded the document onto the server.'));
-    return new Doc<U,T>();
+    return new Doc<M,P,K>();
 }
 //this binding is intended for the lsp to use to get analysis report without affecting the ipc server
 export async function resolveDoc(filePath:string,outputFolder?:string | NoOutput):Promise<ResolutionResult | undefined> {
@@ -64,28 +64,27 @@ export async function genTypes<K extends string>(docName:string,outputFolder:str
     const union = ' | ';
     const terminator = ";\n";
     const exportType = (name:string):string=>`export type ${name} =`;
-    const arr = (elementType:unknown):string =>`(${elementType})[]`;
 
     const typeFile = docName + '.types.ts';
     const typeFilePath = path.join(outputFolder,typeFile);
     
     const memberUnion =  (await doc.allMembers()).map(member=>`"${member}"`).join(union);
-    const memberDeclaration = `${exportType('members')} ${arr(memberUnion)}`;
+    const memberDeclaration = `${exportType('members')} ${memberUnion}`;
     await fs.writeFile(typeFilePath,memberDeclaration + terminator);
 
-    const relations = new Set<string>();
+    const predicates = new Set<string>();
     Object.entries(await doc.aliases()).forEach(([alias,predicate])=>{
-        relations.add(alias);
-        relations.add(predicate);
+        predicates.add(alias);
+        predicates.add(predicate);
     });
 
-    const relationsUnion = Array.from(relations).map(relationship=>`"${relationship}"`).join(union);
-    const relationsDeclaration = `${exportType('relations')} ${relationsUnion}`;
-    await fs.appendFile(typeFilePath,relationsDeclaration + terminator);
+    const predicatesUnion = Array.from(predicates).map(predicate=>`"${predicate}"`).join(union);
+    const predicatesDeclaration = `${exportType('predicates')} ${predicatesUnion}`;
+    await fs.appendFile(typeFilePath,predicatesDeclaration + terminator);
 
     if (rules) {
         const rulesUnion = (rules)?Object.keys(rules).map(rKey=>`"${rKey}"`).join(union):'';
-        const rulesDeclaration = `${exportType('rulesUnion')} ${rulesUnion}`;
+        const rulesDeclaration = `${exportType('keyofRules')} ${rulesUnion}`;
         await fs.appendFile(typeFilePath,rulesDeclaration + terminator);
     }
     console.log(chalk.green('Sucessfully generated the types at: '),typeFilePath);
@@ -96,18 +95,25 @@ export async function setupOutput<K extends string>(srcFilePath:string,outputFol
     const docName = path.basename(srcFilePath,path.extname(srcFilePath));
     if (doc) await genTypes(docName,outputFolder,doc,rules);
 }
-export class Doc<U extends string=string,T extends PatternedAtomList=PatternedAtomList> {//i used arrow methods so that i can have these methods as properties on the object rather than methods.this will allow for patterns like spreading    
+export class Doc<
+    M extends Atom = Atom,//these are the Members of the document.Its a uniontype
+    P extends string=string,//these are the Predicates or aliases.
+    K extends string=string,//the union of all the Keys in a rule
+    Q extends string = P | K,//this a relation Query.it can either query for (Predicates/aliases) or a rule Key
+    L =AtomList<M>//the List of all the members
+    > {
+    //i used arrow methods so that i can have these methods as properties on the object rather than methods.this will allow for patterns like spreading    
     //this method allows the user to query for the truthiness of a statement of a rule the same way they do with facts.So that rather than calling methods directly on the rule object,they write the name of the rule they want to check against as they would for fact querying and this method will forward it to the correct rule by key.It also includes aliases allowing users to also query rules with aliases that will still forward to the correct rule even though the rule's name isnt the alias.
     //this is recommended to use for querying rather direct function calls on a rule object but use the rule object to directly build functions or other rules for better type safety and control and use this mainly as a convenience for querying.
     //it will also fallback to direct fact checking if the statement doesnt satisfy any of the given rules making it a good useful utility for querying the document against all known facts and rules with alias support in a single call.Rules will be given priority first over direct fact checking because this method unlike isItAFact is designed for checking with inference.The check mode is used as part of the fallback to fact querying
-    public isItImplied:(relationshipQuery:U,statement:T)=>Promise<boolean> = async ()=>false;
+    public isItImplied:(relation:Q,statement:L)=>Promise<boolean> = async ()=>false;
     
-    public useRules<K extends string>(rules:Record<K,Rule | RecursiveRule>):void {
+    public useRules(rules:Record<K,Rule>):void {
         const rKeys = Object.keys(rules);
-        this.isItImplied = async (relationshipQuery,statement):Promise<boolean> => {//this is a pattern to query rules with the same interface design as querying a fact
+        this.isItImplied = async (relation,statement):Promise<boolean> => {//this is a pattern to query rules with the same interface design as querying a fact
             const aliases = await this.aliases();
             for (const rKey of rKeys) {
-                const queryKey = aliases[relationshipQuery] || relationshipQuery;
+                const queryKey = aliases[relation] || relation;
                 const forwardKey = aliases[rKey] || rKey;
                 const ruleFucntion = rules[rKey as K];
                 if (queryKey === forwardKey) {
@@ -116,25 +122,18 @@ export class Doc<U extends string=string,T extends PatternedAtomList=PatternedAt
             }
             return false;
         };
-    };
-    public isItStated = async(predicate:U,statement:T,checkMode:Check):Promise<boolean>=> {
-        const result:Result.error | boolean = await client.request("isItAFact",{predicate,statement,byMembership:Boolean(checkMode)});
+    };//the reason why i made this to take the relations query Q instead of predicates P is to have full intellisese of all the possible relations to ask regardless if its for a fact or an implication
+    public isItStated = async(relation:Q,statement:L,checkMode:Check):Promise<boolean>=> {
+        const result:Result.error | boolean = await client.request("isItAFact",{predicate:relation,statement,byMembership:Boolean(checkMode)});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
     //It is intended that the members type generated in the output should be passed as T.this will let ts to treat the wildcard as a valid memeber of the document even though it isnt explicitly written
-    public wildCard = async():Promise<T[number]>=>{
-        return (await client.request('wildCard',{})) as T[number];//this one can not return a doc error because its a static property thats always available on the server
+    public wildCard = async():Promise<M>=>{
+        return (await client.request('wildCard',{})) as M;//this one can not return a doc error because its a static property thats always available on the server
     };
-    public printAnswer(answer:boolean):void {
-        const text = (answer)?chalk.green('yes'):chalk.red('no');
-        console.log(chalk.yellow('\nAnswer: '),text);
-    }
-    private static throwDocError():never {
-        throw new Error(chalk.red('The fact checker was unable to load to the document.'));
-    }
-    public allMembers = async ():Promise<AtomList>=>{
-        const result:Result.error | AtomList = await client.request('allMembers',{});
+    public allMembers = async ():Promise<AtomList<M>>=>{
+        const result:Result.error | AtomList<M> = await client.request('allMembers',{});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
@@ -143,43 +142,46 @@ export class Doc<U extends string=string,T extends PatternedAtomList=PatternedAt
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
-    public findAllFacts = async (predicate:string,statement:PatternedAtomList,checkMode:Check):Promise<AtomList[]>=>{
-        const result:Result.error | AtomList[] = await client.request("findAllFacts",{predicate,statement,byMembership:Boolean(checkMode)});
+    public findAllFacts = async (predicate:P,statement:L,checkMode:Check):Promise<AtomList<M>[]>=>{
+        const result:Result.error | AtomList<M>[] = await client.request("findAllFacts",{predicate,statement,byMembership:Boolean(checkMode)});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
-    public findFirstNFacts = async (num:number,predicate:string,statement:PatternedAtomList,checkMode:Check):Promise<AtomList[]>=> {
-        const result:Result.error | AtomList[] = await client.request("findFirstNFacts",{num,predicate,statement,byMembership:Boolean(checkMode)});
+    public findFirstNFacts = async (num:number,predicate:P,statement:L,checkMode:Check):Promise<AtomList<M>[]>=> {
+        const result:Result.error | AtomList<M>[] = await client.request("findFirstNFacts",{num,predicate,statement,byMembership:Boolean(checkMode)});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
-    public genCandidates = async <T extends Atom,N extends number>(howManyToReturn:N,predicate:string,inputCombination:Atom[],visitedCombinations:string[]):Promise<GeneratedCandidates<T>>=>{
-        const result:Result.error | GeneratedCandidates<T> =  await client.request("genCandidates",{howManyToReturn,predicate,inputCombination,visitedCombinations});
+    public genCandidates = async <N extends number>(howManyToReturn:N,predicate:P,inputCombination:Atom[],visitedCombinations:string[]):Promise<GeneratedCandidates<M>>=>{
+        const result:Result.error | GeneratedCandidates<M> =  await client.request("genCandidates",{howManyToReturn,predicate,inputCombination,visitedCombinations});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
-    public selectSmallestRecord = async (predicates:string[]):Promise<string>=> {
-        const result:Result.error | string = await client.request('selectSmallestRecord',{predicates});
+    public selectSmallestRecord = async (predicates:P[]):Promise<P>=> {
+        const result:Result.error | P = await client.request('selectSmallestRecord',{predicates});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
-    public intersection = async (arrays:(Atom | undefined)[][]):Promise<Atom[]>=> {
-        const result:Result.error | Atom[] =  await client.request('intersection',{arrays});
+    public intersection = async (arrays:(Atom<M> | undefined)[][]):Promise<Atom<M>[]>=> {
+        const result:Result.error | Atom<M>[] =  await client.request('intersection',{arrays});
         if (result === Result.error) Doc.throwDocError();
         return result;
     };
+    public printAnswer(answer:boolean):void {
+        const text = (answer)?chalk.green('yes'):chalk.red('no');
+        console.log(chalk.yellow('\nAnswer: '),text);
+    }
+    private static throwDocError():never {
+        throw new Error(chalk.red('The fact checker was unable to load to the document.'));
+    }
 }
 export type Rule = ProceduralRule | RecursiveRule;
-export type ProceduralRule<T=any> = (doc:Doc,statement:T)=>Promise<boolean>;
-export type RecursiveRule<T=any> = (doc:Doc,statement:T,visitedCombinations:string[])=>Promise<boolean>;
+export type ProceduralRule<T extends AtomList<Atom>=any> = (doc:Doc,statement:T)=>Promise<boolean>;
+export type RecursiveRule<T extends AtomList<Atom>=any> = (doc:Doc,statement:T,visitedCombinations:string[])=>Promise<boolean>;
 
-export type WildCard = string;
-export type Atom = string | number;
-export type AtomList = Atom[];
-export type PatternedAtomList = AddUnionToElements<AtomList,WildCard>;
-export type AddUnionToElements<T extends readonly any[], U> = {
-    [K in keyof T]: T[K] | U;
-};
+export type Atom<T extends string | number = string | number> = T;
+export type AtomList<T extends string | number> = Atom<T>[];
+
 export enum Result {
     success='success',
     error='error'
@@ -195,7 +197,7 @@ export interface ResolutionResult {
     result:Result,
     jsonPath:string | NoOutput | undefined,
 }
-export interface GeneratedCandidates<T> {
-    candidates:T[][],
+export interface GeneratedCandidates<T extends string | number> {
+    candidates:AtomList<T>[],
     checkedCombinations:string[]
 }
