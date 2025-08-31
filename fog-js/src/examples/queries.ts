5331b8788d9a5eb89e7c4e8d55a54701c50381d9
The-BigMan-tech <saddlebox9@gmail.com>
2025-08-31 10:18:34 +0100

    i decided to remove the recurson unsafe code

--- fog-js/src/examples/queries.ts
+++ fog-js/src/examples/queries.ts
@@ -15,18 +15,3 @@ doc.printAnswer(answer);
 
 const wildCard = await doc.wildCard();
 console.log(await doc.findAllFacts('allies',["Cole",wildCard],checkBy.Membership));
-
-//Do not use this in recursive functions.its meant to be used procedurally.directly use the generateCandidates method for recursive methods and provide the input and checked combinations to it so as to prevent cycles.
-const runWithCandidates = async(predicate:P,func:(...args:M[])=>Promise<any>):Promise<void> =>{
-    const {combinations} = await doc.genCandidates(func.length,predicate,[],[]);
-    for (const combination of combinations) {
-        await func(...combination);
-    }
-};
-await runWithCandidates("friends",async (A,B)=>{
-    const y = await doc.isItImplied('allies',[A,B]);
-    if (y && await doc.isItStated("male",[B],checkBy.Membership)) {
-        console.log(A);
-    }
-    return;
-});
