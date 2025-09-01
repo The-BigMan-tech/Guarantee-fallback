cd121ebb5116537b4ab7e847f76f4a2be332bbeb
The-BigMan-tech <saddlebox9@gmail.com>
2025-09-01 10:38:48 +0100

    finished the rule error handling

--- fog-js/src/main.ts
+++ fog-js/src/main.ts
@@ -131,11 +131,11 @@ export class Doc<//i used an empty string over the string type for better type s
     //this method allows the user to query for the truthiness of a statement of a rule the same way they do with facts.So that rather than calling methods directly on the rule object,they write the name of the rule they want to check against as they would for fact querying and this method will forward it to the correct rule by key.It also includes aliases allowing users to also query rules with aliases that will still forward to the correct rule even though the rule's name isnt the alias.
     //this is recommended to use for querying rather direct function calls on a rule object but use the rule object to directly build functions or other rules for better type safety and control and use this mainly as a convenience for querying.
     //it will also fallback to direct fact checking if the statement doesnt satisfy any of the given rules making it a good useful utility for querying the document against all known facts and rules with alias support in a single call.Rules will be given priority first over direct fact checking because this method unlike isItAFact is designed for checking with inference.The check mode is used as part of the fallback to fact querying
-    public isItImplied:(relation:P | R,statement:L,fallback?:FactCheckMode)=>Promise<boolean | undefined> = async ()=>false;
+    public isItImplied:(relation:P | R,statement:L,fallback?:FactCheckMode)=>Promise<boolean | Result.error> = async ()=>false;
     
     public useRules(rules:Record<R,Rule<P>>):void {
         const rKeys = Object.keys(rules);
-        this.isItImplied = async (relation,statement,fallback?:FactCheckMode):Promise<boolean | undefined> => {//this is a pattern to query rules with the same interface design as querying a fact
+        this.isItImplied = async (relation,statement,fallback?:FactCheckMode):Promise<boolean | Result.error> => {//this is a pattern to query rules with the same interface design as querying a fact
             const predicates = await this.predicates();
             for (const rKey of rKeys) {
                 const queryKey = predicates[relation] || relation;
@@ -148,13 +148,12 @@ export class Doc<//i used an empty string over the string type for better type s
                         if (err instanceof ZodError) {
                             const errors = JSON.parse(err.message) as Error[];
                             const error = '\n' + errors.map(error=>'-' + error.message + '.').join('\n');
-                            console.log(error);
-                            console.log(chalk.red.underline('\nStatement validation error:'),error);
+                            console.log(chalk.red.underline('\nStatement validation error:'),error,'\n');
                         }else {
-                            console.log(chalk.red(`The rule '${rKey}' encountered an error`));
-                            console.log(err);
+                            console.log(chalk.red(`\nThe rule '${rKey}' encountered an error:`));
+                            console.log((err as Error).message + '\n');
                         }
-                        return;
+                        return Result.error;
                     }
                 }
             }
@@ -233,7 +232,7 @@ export type FactCheckMode = boolean;
 export const checkBy = factCheckModes;//to be used in the isItStated method
 export const fallbackTo = factCheckModes;//to be used in in the isItImplied method for clarity that the implication check fallbacks to fact checking if the statement isnt explicitly said to be true by a rule.This is more clear than writing check by ... which is because the check mode the caller passes to the implication check doesnt in any way,affect the actual implication process because its explicitly handled by the rules.
 
-enum Result {
+export enum Result {
     success='success',
     error='error'
 }
