6f6adb89d67d4fdb0814f890ad7226f3e900383a
The-BigMan-tech <saddlebox9@gmail.com>
2025-09-02 10:24:23 +0100

    updated my pkg.json

--- fog-js/src/examples/documents/output/doc.types.ts
+++ fog-js/src/examples/documents/output/doc.types.ts
@@ -1,4 +1,4 @@
-export type members = "Billy" | "John" | "Mark" | "Zane" | "Cole" | "Leo" | "Susan" | "Matt" | "Philip" | "Mandy" | "9" | "shoes";
+export type members = "Billy" | "John" | "Zane" | "Cole" | "Leo" | "Mark" | "Susan" | "Matt" | "Philip" | "Mandy" | 9 | "shoes";
 export type predicates = "allies" | "friends" | "father" | "parent" | "female" | "friend" | "has" | "male" | "males" | "mother";
 export type keyofRules = "directFriends" | "indirectFriends" | "friends" | "siblings" | "brothers";
 export interface info {
