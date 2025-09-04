f6552fcbf5dc23340015703e2dc0a268e94813aa
The-BigMan-tech <saddlebox9@gmail.com>
2025-09-04 15:54:38 +0100

    creating my lsp

---
+++ fog-support/src/lsp-server/server.ts
@@ -0,0 +1,26 @@
+import {
+    createConnection,
+    ProposedFeatures,
+    TextDocuments,
+    TextDocumentSyncKind
+} from 'vscode-languageserver/node';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+const connection = createConnection(ProposedFeatures.all);
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+connection.onInitialize(() => {
+    return {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental
+        }
+    };
+});
+
+documents.onDidChangeContent(change => {
+    const text = change.document.getText();
+    connection.console.log(`Document changed. Current length: ${text.length}`);
+});
+
+documents.listen(connection);
+connection.listen();
