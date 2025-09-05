c0c458fce62eb8212e2e0f16325879980d041685
The-BigMan-tech <saddlebox9@gmail.com>
2025-09-05 22:48:36 +0100

    made my ext to use my lsp

--- fog-support/src/lsp-server/server.ts
+++ fog-support/src/lsp-server/server.ts
@@ -5,7 +5,7 @@ import {
     TextDocumentSyncKind
 } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
-
+import {analyzeDocument} from "fog-js";
 
 const connection = createConnection(ProposedFeatures.all);
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
@@ -20,6 +20,13 @@ connection.onInitialize(() => {
 
 documents.onDidChangeContent(change => {
     const text = change.document.getText();
+    analyzeDocument(text).then(analysis=>{
+        console.log('🚀 => :24 => analysis:',analysis);
+        connection.sendDiagnostics({
+            uri: change.document.uri,
+            diagnostics: analysis.diagnostics
+        });
+    });
     connection.console.log(`Document changed. Current length: ${text.length} text: ${text}`);
 });
 
