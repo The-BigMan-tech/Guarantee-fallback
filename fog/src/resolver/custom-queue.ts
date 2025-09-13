defa660b3a5b255e269b3623c2eb1dee3afb50e8
The-BigMan-tech <saddlebox9@gmail.com>
2025-09-13 21:17:42 +0100

    added O1 unshifting to my dequeu

--- fog/src/resolver/custom-queue.ts
+++ fog/src/resolver/custom-queue.ts
@@ -2,15 +2,25 @@ export default class CustomQueue<T> {//i made this q because denque diesnt allow
     private arr:(T | undefined)[];
     private start:number;
     private compactionFraction:number = 0.40;//a weight between 0 and 1
+    private initHeadSpace:number = 5;
 
     constructor(init?:T[]) {
         this.arr = [];
-        this.start = 0;
+        this.start = this.initHeadSpace;
+        this.allocateHeadSpace(this.initHeadSpace);
         if (init) {
             for (const element of init) {
                 this.push(element);
             }
         }
+    }
+    private allocateHeadSpace(size:number):void {
+        const newArr = new Array(size + this.arr.length);
+        for (let i=0; i < this.arr.length; i++ ) {
+            newArr[size + i] = this.arr[i];
+        }
+        this.arr = newArr;
+        this.start += size;
     }
     public push(element:T):void {//O1
         this.arr.push(element);
@@ -27,35 +37,38 @@ export default class CustomQueue<T> {//i made this q because denque diesnt allow
         this.compactIfLarge();
         return value;
     }
+    private insert(element:T):void {
+        this.start--;
+        this.arr[this.start] = element;
+    }
     public unshift(element:T):void {//O(n) or O1
         if (this.start === 0) {
-            this.arr.unshift(element);
+            this.allocateHeadSpace(this.arr.length);
+            this.insert(element);
         }else {
-            this.start--;
-            this.arr[this.start] = element;
+            this.insert(element);
         }
     }
     public get(i:number):T | undefined {//O1
+        if ((i < 0) || (i >= this.length)) return undefined;
         const index = this.start + i;
-        if ((index >= this.arr.length) || (index < this.start)) return undefined;
         return this.arr[index] as T;
     }
     public set(i:number,element:T):boolean {//O1
+        if ((i < 0) || (i >= this.length)) return false;
         const index = this.start + i;
-        if ((index >= this.arr.length) || (index < this.start)) return false;
         this.arr[index] = element;
         return true;
     }
     public array():T[] {//O(n)
         return this.arr.slice(this.start) as T[];
     }
-    public length():number {//O1
+    public get length():number {//O1
         return this.arr.length - this.start;
     }
     private compactIfLarge():void {//O(n)
         const reachedThreshold = (this.start / this.arr.length) >= this.compactionFraction;
         if ((this.start > 0) && reachedThreshold) {
-            console.log('reached thresh');
             this.arr = this.arr.slice(this.start);
             this.start = 0;
         }
