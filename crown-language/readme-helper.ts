6e2b93638b9469aa654a2e286c1fc7606453cb84
The-BigMan-tech <saddlebox9@gmail.com>
2025-09-24 16:09:25 +0100

    upgraded it

--- crown-language/readme-helper.ts
+++ crown-language/readme-helper.ts
@@ -3,6 +3,18 @@ import MarkdownIt from "markdown-it";
 import fs from "fs/promises";
 import grammar from "./syntaxes/crown.tmLanguage.json" with {type:'json'};
 import crownTokenColors from "./syntaxes/crown-token-colors.json" with {type:"json"};
+import chalk from "chalk";
+import chokidar from 'chokidar';
+
+const rawMd = './readme.raw.md';
+// Initialize watcher.
+const watcher = chokidar.watch(rawMd, {
+    persistent: true,
+    ignoreInitial: true,    // don't fire events for files already present on start
+    awaitWriteFinish: true, // wait for writes to finish before emitting events
+    ignored: /(^|[/\\])\../, // ignore dotfiles like .git etc.
+    depth: 99               // watch subdirectories recursively (adjust depth as needed)
+});
 
 const highlighter = await createHighlighter({
     themes:[crownTokenColors], // Specify your desired theme
@@ -13,6 +25,7 @@ const highlighter = await createHighlighter({
         }
     ], // Specify languages you need
 });
+
 const md = new MarkdownIt({
     highlight:(code,lang)=>{
         if (lang === "crown") {
@@ -24,6 +37,19 @@ const md = new MarkdownIt({
         return '<pre><code>' + md.utils.escapeHtml(code) + '</code></pre>';
     }
 })
-const mdContent = await fs.readFile('./readme.raw.md','utf-8');
-const result = md.render(mdContent);
-await fs.writeFile('./README.md',result);
+
+async function genMd() {
+    const mdContent = await fs.readFile(rawMd,'utf-8');
+    const result = md.render(mdContent);
+    
+    await fs.writeFile('./README.md',result);
+    console.log(chalk.green('Successfully outputted the markdown','\n'));
+}
+
+watcher.on('change',async (filePath) => {
+    console.log(chalk.yellow('Detected md change:'), filePath,'\n');
+    await genMd();
+    console.log(chalk.blue('Watching the raw md file...\n'));
+});
+
+console.log(chalk.blue('Watching the raw md file...\n'));
